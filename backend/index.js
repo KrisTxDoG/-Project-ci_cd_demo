028d2318e5b493d14e3d8558a4816fcee60b3efc
unknown <cysh@kristcai-asus.bankpro.com.tw>
2025-08-13 14:22:54 +0800

    Delete !!!!!

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello CI/CD World!!!!!??????' });
+  res.json({ message: 'Hello CI/CD World!' });
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
